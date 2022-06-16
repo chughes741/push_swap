50c11c4d6334c4a24c6662f31328cb505dd5a57e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-06-16 10:47:47 -0400

    Added: free_sets to header

--- push_swap.h
+++ push_swap.h
@@ -21,6 +21,6 @@ int		*get_pattern(int *sorted_set, int *unsorted_set);
 int		*parse_args(int argc, char **argv);
 void	print_pattern(int *sort_pattern);
 int		*sort_args(int argc, int *unsorted_set);
-
+void	free_sets(int **unsorted_set, int **sorted_set, int **sort_pattern);
 
 #endif
