c451f9985258dc5fceb8db5dfbd6e5ed357b1d2e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-06-18 15:21:03 -0400

    Added a struct to push_swap

--- src/push_swap.c
+++ src/push_swap.c
@@ -13,43 +13,58 @@
 #include "../push_swap.h"
 #include <stdio.h>
 
-void	replace_values(unsigned *array, unsigned argc)
+typedef struct t_array {
+	int			*i_args;
+	unsigned	*u_args;
+}				t_array;
+
+unsigned	*itou(int *array, int n)
+{
+	unsigned	*rtn_array;
+	int			i;
+
+	i = -1;
+	rtn_array = ft_calloc(n, sizeof(unsigned));
+	while (++i < n)
+		rtn_array[i] = (array[i] ^ (1 << 31));
+	return (rtn_array);
+}
+
+void	order_values(unsigned *array, unsigned n)
 {
 	unsigned	i;
 	unsigned	j;
 	unsigned	min_val;
 
 	i = -1;
-	while (++i < argc)
-		array[i] ^= 1 << 31;
-	j = -1;
-	while (++j <= argc)
+	while (++i <= n)
 	{
 		min_val = UINT32_MAX;
-		i = -1;
-		while (++i < argc)
+		j = -1;
+		while (++j < n)
 		{
-			if (array[i] > j + 1 && array[i] < min_val)
-				min_val = array[i];
+			if (array[j] > i + 1 && array[j] < min_val)
+				min_val = array[j];
 		}
-		i = -1;
-		while (++i < argc)
+		j = -1;
+		while (++j < n)
 		{
-			if (array[i] == min_val)
-				array[i] = j;
+			if (array[j] == min_val)
+				array[j] = i;
 		}
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	int	*array;
+	static t_array	array;
 
 	if (input_check(argc, argv) != 0)
 		exit(0);
-	array = parse_args(argc, argv); // TODO change to utoa
-	replace_values((unsigned *)array, (unsigned)argc - 1);
+	array.i_args = parse_args(argc, argv);
+	array.u_args = itou(array.i_args, argc - 1);
+	order_values(array.u_args, (unsigned)argc - 1);
 	for (int i = 0; i < argc - 1; ++i)
-		printf("%i\n", array[i]);
+		printf("%u\n", array.u_args[i]);
 	return (0);
 }
