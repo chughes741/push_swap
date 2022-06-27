3ee3e434bc05f2cd109ce6c917bf30ddc8eb6e69
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-06-27 15:03:46 -0400

    Added DEBUG ifndef to print moves in main

--- src/push_swap.c
+++ src/push_swap.c
@@ -20,6 +20,8 @@ int	main(int argc, char *argv[])
 	set_up(argc, argv);
 	list_moves();
 	data = get_data();
-	printf("%s", data->moves);
+	#ifndef DEBUG
+		printf("%s", data->moves);
+	#endif
 	return (0);
 }
