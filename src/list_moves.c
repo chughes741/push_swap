c9305b7b51567241bce1e1fae648230109ea77c5
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-06-27 17:35:00 -0400

    Changed to insertion swap

--- src/list_moves.c
+++ src/list_moves.c
@@ -12,122 +12,26 @@
 
 #include "../push_swap.h"
 
-
-static void	split_stack(void)
+void	insertion_sort(void)
 {
 	t_data	*data;
-	int	i;
-	int	half_length;
 
 	data = get_data();
-	i = 0;
-	half_length = data->n_a / 2;
-	while (i < half_length)
+	while (data->n_a)
 	{
-		if (data->stack_a[0] < half_length)
-		{
+		if (lowest_a() == 0)
 			push_b();
-			i++;
-		}
-		else if (data->stack_a[0] > data->stack_a[1] && lowest_a() != 1)
-		{
-			if (data->stack_b[0] < data->stack_b[1] && highest_b() != 1)
-				swap_both();
-			else
-				swap_a(0);
-		}
-		r_rotate_a(0);
-	}
-	return ;
-}
-
-static void	swap_sort_a(void)
-{
-	t_data	*data;
-
-	data = get_data();
-	while (in_order_a())
-	{
-		if (data->stack_a[0] > data->stack_a[1] && lowest_a() != 1)
-		{
-			swap_a(0);
-			r_rotate_a(0);
-		}
 		else
 			rotate_a(0);
 	}
-	return ;
-}
-
-static void	swap_sort_b(void)
-{
-	return;
-	t_data	*data;
-
-	data = get_data();
-	while (in_order_b())
-	{
-		if (data->stack_b[0] == data->stack_a[0] - 1)
-			push_a();
-		if (data->stack_b[0] < data->stack_b[1] && highest_b() != 1)
-		{
-			swap_b(0);
-			r_rotate_b(0);
-		}
-		else
-			rotate_b(0);
-	}
-	return ;
-}
-
-static void	swap_sort(void)
-{
-	t_data	*data;
-
-	data = get_data();
-	while (in_order_a() || in_order_b())
-	{
-		if ((data->stack_a[0] > data->stack_a[1] && lowest_a() != 1) &&\
-		(data->stack_b[0] < data->stack_b[1] && highest_b() != 1))
-		{
-			swap_both();
-			r_rotate_both();
-		}
-		else if (data->stack_a[0] > data->stack_a[1] && lowest_a() != 1)
-		{
-			swap_a(0);
-			r_rotate_a(0);
-			rotate_b(0);
-		}
-		else if (data->stack_b[0] < data->stack_b[1] && highest_b() != 1)
-		{
-			swap_b(0);
-			r_rotate_b(0);
-			rotate_a(0);
-		}
-		else
-			rotate_both();
-	}
-}
-
-static void	merge_stack(void)
-{
-	t_data	*data;
-	int	i;
-
-	data = get_data();
-	i = data->n_b + 1;
-	while (--i)
+	while (data->n_b)
 		push_a();
 	return ;
 }
 
+
 void	list_moves(void)
 {
-	split_stack();
-	swap_sort();
-	swap_sort_b();
-	merge_stack();
-	swap_sort_a();
+	insertion_sort();
 	return ;
 }
