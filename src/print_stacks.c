3191a1205824860fea4dedcc4321afad98a2a3c7
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-06-27 15:02:59 -0400

    Cahnged print_stack to only run when DEBUG is def

--- src/print_stacks.c
+++ src/print_stacks.c
@@ -2,22 +2,24 @@
 #include "../push_swap.h"
 
 void	print_stacks(void) {
-	return;
-	t_data *data = get_data();
+	#ifdef DEBUG
+		t_data *data = get_data();
 
-	sleep(1);
-	printf("\e[1;1H\e[2J");
-	printf("  A | B  \n");
-	printf("---------\n");
-	for (int i = 0; i < 17; ++i) {
-		if (i < data->n_a)
-			printf(" %2i |", data->stack_a[i]);
-		else
-			printf("  - |");
-		if (i < data->n_b)
-			printf(" %-2i \n", data->stack_b[i]);
-		else
-			printf(" -  \n");
-	}
-	printf("\n");
+		sleep(1);
+		printf("\e[1;1H\e[2J");
+		printf("  A | B  \n");
+		printf("---------\n");
+		for (int i = 0; i < 25; ++i) {
+			if (i < data->n_a)
+				printf(" %2i |", data->stack_a[i]);
+			else
+				printf("  - |");
+			if (i < data->n_b)
+				printf(" %-2i \n", data->stack_b[i]);
+			else
+				printf(" -  \n");
+		}
+		printf("\n");
+	#endif
+	return;
 }
