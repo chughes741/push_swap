a4bd07a97c01e5c888361cbc694793fd403d9616
Cole - MBA <coltenhughes@gmail.com>
2022-06-27 22:08:18 -0400

    Added a shell script to log test results

---
+++ logtests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+temp_log=$(mktemp)
+RESULTS="results.txt"
+
+COUNT=100
+SETS=( 10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500)
+
+rm $RESULTS
+touch $RESULTS
+
+for i in ${SETS[@]}
+do 
+	printf "Testing %d random numbers %d times\n" $i $COUNT
+	bash ./loop.sh $i $COUNT  1>>${temp_log}
+	grep 'average instructions' ${temp_log} | tail -1 >> $RESULTS
+done
+
+rm ${temp_log}
