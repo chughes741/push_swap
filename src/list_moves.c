6402cbb5cdb17f628627d6a495a744f7d3071710
Cole - MBA <coltenhughes@gmail.com>
2022-06-29 20:19:38 -0400

    Altered list_moves for index pointer

--- src/list_moves.c
+++ src/list_moves.c
@@ -19,9 +19,9 @@ void	insertion_sort(void)
 	data = get_data();
 	while (data->n_a)
 	{
-		while (lowest_a() != 0)
+		while (lowest_a() != data->top_a)
 		{
-			if (highest_a() != 0)
+			if (highest_a() != data->top_a)
 			{
 				if (lowest_a() < data->n_a / 2)
 					rotate_a(0);
@@ -36,7 +36,7 @@ void	insertion_sort(void)
 		}
 		push_b();
 	}
-	while (highest_b() != 0)
+	while (highest_b() != data->top_b)
 		r_rotate_b(0);
 	while (data->n_b)
 		push_a();
