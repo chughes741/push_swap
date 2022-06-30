cf7148dee4ad235fef05297ada4610d0a64ee9d8
Cole - MBA <coltenhughes@gmail.com>
2022-06-30 19:19:52 -0400

    Removed old insert sort

--- src/list_moves.c
+++ src/list_moves.c
@@ -61,38 +61,6 @@ void	insert_to_a(void)
 	}
 }
 
-void	insert_sort(void)
-{
-	return ; // ! TESTING
-	t_data	*data;
-
-	data = get_data();
-	while (data->n_a)
-	{
-		while (lowest_a() != 0)
-		{
-			if (highest_a() != 0)
-			{
-				if (lowest_a() < data->n_a / 2)
-					rotate_a(0);
-				else
-					r_rotate_a(0);
-			}
-			else
-			{
-				push_b();
-				rotate_b(0);
-			}
-		}
-		push_b();
-	}
-	while (highest_b() != 0)
-		r_rotate_b(0);
-	while (data->n_b)
-		push_a();
-	return ;
-}
-
 void	list_moves(void)
 {
 	t_data	*data;
